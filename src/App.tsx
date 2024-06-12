9810364c120dbc244fbb95ff912d54122eeb5d26
Шухов Владислав <vladislav.shukhov@ic-group.ru>
2024-06-12 11:55:51 +0300

    added todo logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './app/router';
+import { router } from 'app';
 
 function App() {
   return (
---
+++ src/modules/DreamTime/index.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export const DreamTime = (props: Props) => {
+  return <div>DreamTime</div>;
+};
