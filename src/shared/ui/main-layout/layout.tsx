b217c7b402e1c2ef5f29a4c0f3885714e58f1f7e
Шухов Владислав <vladislav.shukhov@ic-group.ru>
2024-06-24 16:51:19 +0300

    remaded layout

--- src/shared/ui/main-layout/layout.tsx
+++ src/shared/ui/main-layout/layout.tsx
@@ -1,8 +1,9 @@
-import { Layout, theme } from 'antd';
+import { Layout, Skeleton, Space, theme } from 'antd';
 import { Header } from '../header';
 import { Sidebar } from '../sidebar';
 import { sidebarItems } from 'shared';
 import styles from './layout.module.scss';
+import { useState } from 'react';
 
 const { Content } = Layout;
 
@@ -10,24 +11,43 @@ type Props = {
   children: React.ReactNode;
 };
 
+type SizeType = 'default' | 'small' | 'large';
+type ButtonShapeType = 'circle' | 'square' | 'round' | 'default';
+type AvatarShapeType = 'circle' | 'square';
+
 export const MainLayout = ({ children }: Props) => {
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  const [size, setSize] = useState<SizeType>('default');
+  const [buttonShape, setButtonShape] = useState<ButtonShapeType>('default');
+  const [avatarShape, setAvatarShape] = useState<AvatarShapeType>('circle');
+  const [block, setBlock] = useState(false);
+
   return (
     <>
       <Layout className={styles.layout}>
         <Header background={colorBgContainer}>
-          <>
-            {/* <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb> */}
-          </>
+          <div className={styles.header__container}>
+            <div></div>
+            <Space>
+              <Skeleton.Button
+                active={false}
+                size={size}
+                shape={buttonShape}
+                block={block}
+              />
+              <Skeleton.Avatar active={false} size={size} shape={avatarShape} />
+              {/* <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+              </Breadcrumb> */}
+            </Space>
+          </div>
         </Header>
-        <Layout>
+        <Layout className={styles.layoutContentContainer}>
           <Sidebar background={colorBgContainer} menuItems={sidebarItems} />
           <Layout className={styles.content__layout}>
             <Content
